ac51f8ed43094e2903a2026d881b12dfea931ff5
DmpDaniel <danielparaiso214@gmail.com>
2023-04-16 19:36:20 +0800

    Refator some Codes

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let inputText = document.getElementById("input")
 let create = document.getElementById("create")
-let delet = document.getElementById("delete")
+let deleteAll = document.getElementById("delete")
 let list = document.getElementById("listItem")
 let selectAll = document.querySelectorAll("li")
 let numberOfList = 0
 
 list.addEventListener("click", handleItemClick)
 
+create.addEventListener("click", enterKey)
 inputText.addEventListener('keydown', function(event) {
-if (event.keyCode === 13) {
+if (event.key === "Enter") {
 if(inputText.value){
 enterKey(event)
 }
-}
-})
+}})
 
 function enterKey (event){
+  if(inputText.value){
 let text = document.createElement("li")
 let listNum = "item"+ numberOfList
 text.innerHTML =  `<p class=${listNum}>${inputText.value}</p> <button id="deletebtn">Delete</button>`
@@ -30,26 +31,7 @@ textToggle.addEventListener("click", () => {
   textToggle.parentNode.classList.toggle("backDone")
 })
 }
-
-create.addEventListener("click" , () => {
-if(inputText.value){
-
-  let text = document.createElement("li")
-  let listNum = "item"+ numberOfList
-  text.innerHTML =  `<p class=${listNum}>${inputText.value}</p> <button id="deletebtn">Delete</button>`
-  text.classList.add("list")  
-  list.insertBefore(text, list.firstChild)
-  inputText.value = ""
-  numberOfList ++
-  let textToggle = text.firstChild;
-  
-  textToggle.addEventListener("click", () => {
-    textToggle.classList.toggle("done")
-    textToggle.parentNode.classList.toggle("backDone")
-
-    }
-    )}
-})
+}
 
 function handleItemClick(event) {
     const clickedItem = event.target;
@@ -63,14 +45,12 @@ list.removeChild(listItem);
 }
 }
 
-
-
-delet.addEventListener("click", () => {
+deleteAll.addEventListener("click", () => {
    inputText.value = ""
-   let k =  document.querySelectorAll("li")
+   let selectAll =  document.querySelectorAll("li")
 
-for(let i = k.length -1; i>= 0; i--){
+for(let i = selectAll.length -1; i>= 0; i--){
 
-    k[i].parentNode.removeChild(k[i])
-}
+    selectAll[i].parentNode.removeChild(k[i])
+} 
 })
